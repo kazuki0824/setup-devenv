ddf72c7c55228cedcc758ea7d620f6156a64a07e
maleicacid <maleicacid824+dev@gmail.com>
2022-12-10 02:41:14 +0900

    Add rust

--- setup.bash
+++ setup.bash
@@ -6,10 +6,10 @@ echo "load-module module-zeroconf-discover" | sudo tee -a $HOME/.config/pulse/de
 echo "load-module module-raop-discover" | sudo tee -a $HOME/.config/pulse/default.pa.d
 echo "# load-module module-raop-sink" | sudo tee -a $HOME/.config/pulse/default.pa.d
 
-flatpak install spotify 
-yes | pacman -S discord 
+flatpak install spotify -y
+yes | sudo pacman -S discord 
 
-yes | pacman -S avahi pulseaudio-zeroconf extra/pulseaudio-rtp docker docker-compose
+yes | sudo pacman -S avahi pulseaudio-zeroconf extra/pulseaudio-rtp docker docker-compose
 yes | yay -S smartgit  
 
 git clone git@github.com:OUXT-Polaris/ouxt_automation.git
@@ -17,4 +17,6 @@ mkdir new-tv
 cd new-tv/
 git clone git@github.com:kazuki0824/recisdb-rs.git --recursive
 git clone git@github.com:kazuki0824/arib-recorder.git --recursive
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -t stable -y
 
