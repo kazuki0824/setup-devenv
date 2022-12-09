24a9931ec794d9a6b102109f6e3bf9b7aaef448d
maleicacid <maleicacid824+dev@gmail.com>
2022-12-10 01:50:56 +0900

    Init

---
+++ setup.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo ".include /etc/pulse/default.pa" | sudo tee -a $HOME/.config/pulse/default.pa.d
+echo "set-default-sink raop_output.ptvserver.local" | sudo tee -a $HOME/.config/pulse/default.pa.d
+echo "load-module module-zeroconf-discover" | sudo tee -a $HOME/.config/pulse/default.pa.d
+echo "load-module module-raop-discover" | sudo tee -a $HOME/.config/pulse/default.pa.d
+echo "# load-module module-raop-sink" | sudo tee -a $HOME/.config/pulse/default.pa.d
+
+flatpak install spotify 
+yes | sudo pacman -S discord 
+
+yes | sudo pacman -S avahi pulseaudio-zeroconf extra/pulseaudio-rtp docker docker-compose
+yes | sudo  yay -S smartgit  
+
+git clone git@github.com:OUXT-Polaris/ouxt_automation.git
+mkdir new-tv
+cd new-tv/
+git clone git@github.com:kazuki0824/recisdb-rs.git --recursive
+git clone git@github.com:kazuki0824/arib-recorder.git --recursive
+
