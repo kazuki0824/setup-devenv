54cc3cea16e7aebbdea3a93fb3d12e4ce45ec04e
maleicacid <4982384+kazuki0824@users.noreply.github.com>
2023-02-19 23:44:23 +0900

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -12,9 +12,14 @@ sudo pacman -Syu
 yes | sudo pacman -S discord flatpak yay
 flatpak install spotify -y
 
-yes | sudo pacman -S avahi pulseaudio-zeroconf extra/pulseaudio-rtp docker docker-compose
+yes | sudo pacman -S avahi pulseaudio-zeroconf extra/pulseaudio-rtp podman podman-compose
 yes | sudo pacman -S brasero baobab yubikey-manager-qt keybase-gui kbfs
 yes | yay -S smartgit teamviewer
+
+# rootless podman setup
+sudo touch /etc/subuid /etc/subgid
+sudo usermod --add-subuids 100000-165535 --add-subgids 100000-165535 $USER
+podman system migrate
 
 mkdir new-tv
 cd new-tv/
